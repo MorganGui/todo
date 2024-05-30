e0589244a43a8d6524e648e5fb040eb93d233694
MorganGui <morgan_guillemin@hotmail.com>
2024-05-30 16:14:26 +0200

    init

---
+++ front/src/classes/Task.js
@@ -0,0 +1,16 @@
+export default class Task {
+  constructor(id, title, desc, checked) {
+    this.id = id
+    this.title = title
+    this.desc = desc
+    this.checked = checked
+  }
+
+  getAll() {
+
+  }
+
+  save() {
+    
+  }
+}
---
+++ front/src/views/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="d-flex flex-column align-center gap-3 pa-12">
+    <v-card v-for="task of tasks" width="100%" max-width="500px">
+      <v-card-title class="d-flex justify-space-between">
+        {{ task.name }}
+        <v-checkbox v-model="task.checked" />
+      </v-card-title>
+      <v-card-text>
+        {{ task.desc }}
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          id: '0',
+          name: 'task 1',
+          desc: 'blabla',
+          checked: false
+        },
+        {
+          id: '1',
+          name: 'task 2',
+          desc: 'blabla',
+          checked: false
+        },
+        {
+          id: '2',
+          name: 'task 3',
+          desc: 'blabla',
+          checked: false
+        },
+        {
+          id: '3',
+          name: 'task 4',
+          desc: 'blabla',
+          checked: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
